31565ccdf828a0c4eadd539244d60a1883042edc
eneskar <eneskaraslan@gmail.com>
2023-10-02 15:58:55 +0300

    first commit of the app

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+// dom queries
+const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const message = newChatForm.message.value.trim();
+
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
+
+// class instances
+const chatUI = new ChatUI(chatList);
+const chatroom = new Chatroom('general', 'shaun');
+
+// get chats & render
+chatroom.getChats(data => chatUI.render(data));
